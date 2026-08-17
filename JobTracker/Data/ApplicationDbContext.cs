0f947892ae550401da135262bdeb9ac980ea9fec
Simeoncoded <simeonolawore07@gmail.com>
2026-08-17 18:09:01 -0400

    update on ef core and db connection

---
+++ JobTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using JobTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<JobApplication> JobApplications { get; set; }
+    }
+}
