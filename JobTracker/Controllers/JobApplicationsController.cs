a8581a3667f0e99cff508921a789e158818f4b3d
Simeoncoded <simeonolawore07@gmail.com>
2026-08-17 18:34:50 -0400

    update on controller

--- JobTracker/Controllers/JobApplicationsController.cs
+++ JobTracker/Controllers/JobApplicationsController.cs
@@ -1,5 +1,7 @@
-﻿using JobTracker.Models;
+﻿using JobTracker.Data;
+using JobTracker.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobTracker.Controllers
 {
@@ -7,26 +9,40 @@ namespace JobTracker.Controllers
     [ApiController]
     public class JobApplicationsController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public JobApplicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            var applications = new List<JobApplication>
-            {new JobApplication
-            {
-                Id = 1,
-                CompanyName = "Microsoft",
-                JobTitle = "Software Developer",
-                Status = "Applied"
-            },new JobApplication
-            {
-                Id = 2,
-                CompanyName = "Microsoft",
-                JobTitle = "Software Developer",
-                Status = "Applied"
-            }
-        };
+            var applications = _context.JobApplications.ToList();
 
             return Ok(applications);
+        }
+        [HttpPost]
+        public IActionResult Create(JobApplication application)
+        {
+            _context.JobApplications.Add(application);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = application.Id }, application);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var application = _context.JobApplications.Find(id);
+
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(application);
         }
     }
 }
