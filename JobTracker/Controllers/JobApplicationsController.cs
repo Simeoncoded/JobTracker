3f88a1026270983b33d48baca1b9ad4554771cb4
Simeoncoded <simeonolawore07@gmail.com>
2026-08-17 17:27:12 -0400

    update on models

---
+++ JobTracker/Controllers/JobApplicationsController.cs
@@ -0,0 +1,32 @@
+﻿using JobTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobApplicationsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var applications = new List<JobApplication>
+            {new JobApplication
+            {
+                Id = 1,
+                CompanyName = "Microsoft",
+                JobTitle = "Software Developer",
+                Status = "Applied"
+            },new JobApplication
+            {
+                Id = 2,
+                CompanyName = "Microsoft",
+                JobTitle = "Software Developer",
+                Status = "Applied"
+            }
+        };
+
+            return Ok(applications);
+        }
+    }
+}
---
+++ JobTracker/Models/JobApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace JobTracker.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public string JobTitle {  get; set; } = string.Empty;
+        public string Status {  get; set; } = string.Empty;
+    }
+}
