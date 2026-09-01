eb3f9e9a97273726dcb9b25a0ffdad54fefd7600
Simeoncoded <simeonolawore07@gmail.com>
2026-09-01 19:12:29 -0400

    started resume logic

---
+++ JobTracker/Models/Resume.cs
@@ -0,0 +1,10 @@
+﻿namespace JobTracker.Models
+{
+    public class Resume
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string ExtractedText {  get; set; } = string.Empty;
+        public DateTime UploadedDate { get; set; }
+    }
+}
